b978acc5b8a37d04f28a2a82edc080d5670cbe79
Robert Prine <robertprine2@yahoo.com>
2016-04-30 23:01:48 -0400

    initial plus pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+//waits for js to start until after html has loaded
+
+$(document).ready(function(){
+
+	//game object to contain variables and methods
+
+	var game = {
+
+		// question slide object array that contains - question, answer1, answer2, answer3, answer4,
+
+		// correct slide object array contains -
+
+		// incorrect slide object array contains - 
+
+		// variable correct answers
+
+		// variable incorrect answers
+
+		// variable unanswered questions
+
+		// variable reset
+
+		// fill reset variable
+
+		// Start button onclick creates the time remaining object and calls the forloop to start 
+
+		// for loop that contains all the objects
+
+		// timer for each slide
+
+		// if the timer reaches 0 move to the correct answer slide and unanswered count ++
+
+		// timer for correct answer slide, incorrect answer slide, and time out slide on zero moves to the next part of the array
+
+		// if statement for onclick buttons to have class correct yield you got it right page and correct count ++
+
+		// else statemen for onclick buttons to yield you got it wrong and show the right answer page incorrect count ++
+
+		// End slide after for loop that contains: All done, here's how you did!, correct answers:, Incorrect Answers:, Unanswered:, Start Over? (reset button)
+
+		// reset button that calls reset variable
+
+
+	} // end game object
+
+}) //ends ready function
